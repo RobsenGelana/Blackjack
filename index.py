fb7b25be1be90a72546ac4f0f4f381d5b3bf9779
RobsenGelana <robsengelana21@gmail.com>
2022-09-13 21:50:17 +0300

    a while loop that let the user play the game as much as he like

--- index.py
+++ index.py
@@ -72,3 +72,6 @@ def play_game():
   print(f"   Your final hand: {user_cards}, final score: {user_score}")
   print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
   print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+  play_game()
