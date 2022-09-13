43924fe0f8b76d857120b8cdcd388dcee7a07ca4
RobsenGelana <robsengelana21@gmail.com>
2022-09-13 21:45:21 +0300

    creating a function for deal card

---
+++ index.py
@@ -0,0 +1,9 @@
+from art import logo
+import random
+
+
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
