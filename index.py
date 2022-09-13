8549384bba2b51fe2b1df081286b29dbbdfc18be
RobsenGelana <robsengelana21@gmail.com>
2022-09-13 21:48:14 +0300

    creating a function that compare the user card and the computer card

--- index.py
+++ index.py
@@ -16,3 +16,24 @@ def calculate_score(cards):
     cards.remove(11)
     cards.append(1)
   return sum(cards)
+
+def compare(user_score, computer_score):
+  #Bug fix. If you and the computer are both over, you lose.
+  if user_score > 21 and computer_score > 21:
+    return "You went over. You lose 😤"
+
+
+  if user_score == computer_score:
+    return "Draw 🙃"
+  elif computer_score == 0:
+    return "Lose, opponent has Blackjack 😱"
+  elif user_score == 0:
+    return "Win with a Blackjack 😎"
+  elif user_score > 21:
+    return "You went over. You lose 😭"
+  elif computer_score > 21:
+    return "Opponent went over. You win 😁"
+  elif user_score > computer_score:
+    return "You win 😃"
+  else:
+    return "You lose 😤"
